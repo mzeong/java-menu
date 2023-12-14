c219a17ee9a3df6778332b490769462264ce6239
김민정 <mjeong@sookmyung.ac.kr>
2023-12-14 13:03:01 +0900

    feat: 요일에 추천할 카테고리 무작위로 정하기 구현

---
+++ src/main/java/menu/domain/Categories.java
@@ -0,0 +1,43 @@
+package menu.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import menu.util.RandomNumberGenerator;
+
+public class Categories {
+    private final RandomNumberGenerator randomNumberGenerator;
+    private final List<Category> categories = new ArrayList<>();
+
+    public Categories(RandomNumberGenerator randomNumberGenerator) {
+        this.randomNumberGenerator = randomNumberGenerator;
+    }
+
+    public Category selectNew() {
+        Category category;
+        do {
+            category = Category.getByOrder(randomNumberGenerator.generate());
+        } while (isAllSelected(category));
+
+        categories.add(category);
+        return category;
+    }
+
+    private boolean isAllSelected(Category target) {
+        int count = (int) categories.stream()
+                .filter(category -> category == target)
+                .count();
+        if (count >= 2) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[ 카테고리 | %s ]",
+                categories.stream()
+                        .map(Category::toString)
+                        .collect(Collectors.joining(" | ")));
+    }
+}
