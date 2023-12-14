3d577830daa824f16f4b19601a74a2979b19f52d
김민정 <mjeong@sookmyung.ac.kr>
2023-12-14 11:33:43 +0900

    feat: 잘못된 입력이면 다시 입력 받기 구현

--- src/main/java/menu/controller/Controller.java
+++ src/main/java/menu/controller/Controller.java
@@ -1,9 +1,14 @@
 package menu.controller;
 
+import java.util.List;
+import menu.domain.Coach;
+import menu.domain.ExcludeMenus;
+import menu.domain.Menu;
 import menu.view.InputView;
 import menu.view.OutputView;
 
 public class Controller {
+    public static final String ERR_PREFIX = "[ERROR] ";
     private final InputView inputView;
     private final OutputView outputView;
     private final Parser parser;
@@ -14,8 +19,32 @@ public class Controller {
         this.parser = parser;
     }
 
-    public void run() {
-        outputView.printStartMessage();
+    public void init() {
+        List<String> coachNames = getCoachNames();
+        for (String name : coachNames) {
+            ExcludeMenus excludeMenus = getExcludeMenus(name);
+            Coach coach = new Coach(name, excludeMenus);
+        }
+    }
 
+    private List<String> getCoachNames() {
+        while (true) {
+            try {
+                return parser.parseCoachNames(inputView.inputCoachNames());
+            } catch (IllegalArgumentException e) {
+                System.out.println(ERR_PREFIX + e.getMessage());
+            }
+        }
+    }
+
+    private ExcludeMenus getExcludeMenus(String name) {
+        while (true) {
+            try {
+                List<Menu> menus = parser.parseExcludeMenus(inputView.inputExcludeMenus(name));
+                return new ExcludeMenus(menus);
+            } catch (IllegalArgumentException e) {
+                System.out.println(ERR_PREFIX + e.getMessage());
+            }
+        }
     }
 }
