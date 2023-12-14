ccd26f6de929895fd4201be0eeb3a6961cb5ad9d
김민정 <mjeong@sookmyung.ac.kr>
2023-12-14 11:22:11 +0900

    refactor: 못 먹는 메뉴 콤마로 구분하기 구현

--- src/main/java/menu/controller/Parser.java
+++ src/main/java/menu/controller/Parser.java
@@ -3,14 +3,29 @@ package menu.controller;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import menu.domain.Menu;
 
 public class Parser {
     public static final String DELIMITER = ",";
 
-    public List<String> parseComma(String input) {
+    public List<String> parseCoachNames(String input) {
         return Arrays.stream(input.split(DELIMITER))
                 .map(String::trim)
-                .filter(str -> !str.isEmpty())
+                .filter(name -> !name.isEmpty())
                 .collect(Collectors.toList());
+    }
+
+    public List<Menu> parseExcludeMenus(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            return List.of();
+        }
+        try {
+            return Arrays.stream(input.split(DELIMITER))
+                    .map(String::trim)
+                    .map(Menu::valueOf)
+                    .collect(Collectors.toList());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("존재하는 메뉴만 입력해야 합니다.");
+        }
     }
 }
